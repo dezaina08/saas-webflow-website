f210adc884a1eb98b28db8283360c55e4ff81f64
Elvin <elvin08.dez@gmail.com>
2023-08-30 01:39:06 +0800

    Add Feature

--- components/Hero.vue
+++ components/Hero.vue
@@ -13,12 +13,61 @@
                     Tickets All-In-One Primarily Software.
                 </div>
                 <div class="d-flex">
-                    <a href="#" class="primary-btn btn d-none d-lg-block"
+                    <a href="#" class="primary-btn btn d-lg-block"
                         >Get Started</a
                     >
-                    <a href="#" class="secondary-btn btn d-none d-lg-block"
+                    <a href="#" class="secondary-btn btn d-lg-block"
                         >How It Work</a
                     >
+                </div>
+            </div>
+            <div class="hero-features d-flex flex-column align-items-center">
+                <div class="text-semibold">Explore Premium Features</div>
+                <div
+                    class="text-opensans text-center mb-5"
+                    style="max-width: 70%"
+                >
+                    Unique And Powerful Suite Of Software To Run Your Entire
+                    Business, Brought To You By A Company With The Long Term
+                    Vision To Transform The Way You Work.
+                </div>
+                <div class="row d-flex flex-md-row flex-column gap-4">
+                    <div
+                        class="col feature d-flex flex-column align-items-center align-items-md-start"
+                    >
+                        <div>
+                            <img src="/images/FeatureIcon.svg" alt="" />
+                        </div>
+                        <div class="text-medium">Powerful Settings</div>
+                        <div class="text-opensans">
+                            Provide Excellent Customer Service. Answer More
+                            Tickets.
+                        </div>
+                    </div>
+                    <div
+                        class="col feature d-flex flex-column align-items-center align-items-md-start"
+                    >
+                        <div>
+                            <img src="/images/FeatureIcon2.svg" alt="" />
+                        </div>
+                        <div class="text-medium">User Friendly</div>
+                        <div class="text-opensans">
+                            Use Timeline To Plan Projects Right How The Pieces
+                            Fit Together.
+                        </div>
+                    </div>
+                    <div
+                        class="col feature d-flex flex-column align-items-center align-items-md-start"
+                    >
+                        <div>
+                            <img src="/images/FeatureIcon3.svg" alt="" />
+                        </div>
+                        <div class="text-medium">Integrated Platform</div>
+                        <div class="text-opensans">
+                            Vivamus Massa Eleifend Etiam Neque Odio Feugiat
+                            Dolor Est.
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,8 +84,8 @@
         background-position: top right;
 
         @media screen and (max-width: 768px) {
-                background-image: none;
-            }
+            background-image: none;
+        }
 
         .hero-text {
             padding: 100px 0;
@@ -82,11 +131,75 @@
             @media screen and (max-width: 768px) {
                 padding: 30px 0;
                 .primary-text {
-                        font-size: 35px;
-                    }
+                    font-size: 35px;
+                }
                 .secondary-text {
                     font-size: 14px;
                 }
+            }
+        }
+
+        .hero-features {
+            padding: 100px 0;
+            .feature {
+                background: #f4f4f4;
+                border-radius: 10px;
+                padding: 40px 36px;
+            }
+
+            .feature-white {
+                background: #ffffff;
+                border-radius: 10px;
+                padding: 40px 36px;
+            }
+
+            img {
+                size: 55px;
+                padding-bottom: 20px;
+            }
+
+            @media screen and (max-width: 768px) {
+                img {
+                    padding-bottom: 5px;
+                }
+            }
+        }
+
+        .text-semibold {
+            font-family: JostSemiBold;
+            font-size: 40px;
+            line-height: normal;
+            margin-bottom: 24px;
+            color: #303030;
+        }
+
+        .text-medium {
+            font-family: JostMedium;
+            font-size: 30px;
+            line-height: normal;
+            margin-bottom: 24px;
+            color: #303030;
+        }
+
+        .text-opensans {
+            font-family: OpenSansRegular;
+            font-size: 20px;
+            line-height: normal;
+            color: #303030;
+        }
+
+        @media screen and (max-width: 768px) {
+            .text-semibold {
+                font-size: 35px;
+                margin-bottom: 0px;
+            }
+            .text-medium {
+                font-size: 25px;
+                margin-bottom: 0px;
+            }
+            .text-opensans {
+                font-size: 14px;
+                margin-bottom: 0px;
             }
         }
     }
