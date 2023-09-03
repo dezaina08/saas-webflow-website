ba2cb668b59ccaa20b706ea294fc635a56a3e6e1
Elvin <elvin08.dez@gmail.com>
2023-09-03 20:10:39 +0800

    Update: Lead & Project Img

--- components/Lead.vue
+++ components/Lead.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="lead mb-5">
         <div class="container-xxl">
-            <div class="row d-flex flex-md-row p-4 p-md-0 flex-column">
-                <div class="col d-flex">
-
+            <div class="row d-flex flex-md-row p-4 p-md-0 flex-column gap-4">
+                <div class="col d-flex justify-content-center">
+                    <img src="/images/Lead.svg" class="img-fluid" alt="" />
                 </div>
-                <div class="col d-flex flex-column">
+                <div class="col d-flex flex-column justify-content-center">
                     <div class="font-family-jostsemibold primary-color text-3xl text-md-4xl text-center text-md-start mb-2">
                         Allocate Leads and Simplify Lead Management
                     </div>
                     <div
-                        class="font-family-opensansregular secondary-color text-base text-md-xl text-center text-md-start mb-3">
+                        class="font-family-opensansregular secondary-color text-base text-md-xl text-center text-md-start mb-3 mb-md-5">
                         Unique and powerful suite of software to run your entire business, brought to you by a company
                         with the long term vision to transform the way you work.
                     </div>
-                    <div class="rounded-4 border p-2 mb-3">
+                    <div class="rounded-4 border p-2 mb-3 mb-md-5">
                         <div
                             class="font-family-jostsemibold primary-color text-2xl text-md-3xl text-center text-md-start ms-0 ms-md-5">
                             Lead generation
@@ -24,7 +24,7 @@
                             Guide Helps Companies Increase Customer & Agent Satisfaction, Also Reducing Support Costs.
                         </div>
                     </div>
-                    <div class="rounded-4 border p-2 mb-3">
+                    <div class="rounded-4 border p-2">
                         <div
                             class="font-family-jostsemibold primary-color text-2xl text-md-3xl text-center text-md-start ms-0 ms-md-5">
                             Team Email
@@ -47,4 +47,10 @@ export default {
 </script>
 <style lang="scss">
 @import "~/assets/scss/mixins/breakpoints";
+.lead {
+    img {
+        
+        min-width: 1%;
+    }
+}
 </style>
--- components/Project.vue
+++ components/Project.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="project mb-5">
         <div class="container-xxl">
-            <div class="row d-flex flex-md-row p-4 p-md-0 flex-column">
-                <div class="col d-flex flex-column">
+            <div class="row d-flex flex-md-row p-4 p-md-0 flex-column gap-4">
+                <div class="col d-flex flex-column justify-content-center">
                     <div class="font-family-jostsemibold primary-color text-3xl text-md-4xl text-center text-md-start mb-2">
                         Ways A Project Management Tool Can Help Your Team
                     </div>
                     <div
-                        class="font-family-opensansregular secondary-color text-base text-md-xl text-center text-md-start mb-3">
+                        class="font-family-opensansregular secondary-color text-base text-md-xl text-center text-md-start mb-3 mb-md-5">
                         Unique and powerful suite of software to run your entire business, brought to you by a company with
                         the long term vision to transform the way you work.
                     </div>
                     <div class="d-flex flex-md-row flex-column">
-                        <div class="p-2 mb-3">
+                        <div class="p-2 mb-3 mb-md-5">
                             <div
                                 class="font-family-jostsemibold primary-color text-2xl text-md-3xl text-center text-md-start">
                                 CRM Security
@@ -22,7 +22,7 @@
                                 cards and make more sales while chatting
                             </div>
                         </div>
-                        <div class="p-2 mb-3">
+                        <div class="p-2">
                             <div
                                 class="font-family-jostsemibold primary-color text-2xl text-md-3xl text-center text-md-start">
                                 Marketing Hub
@@ -34,8 +34,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex">
-
+                <div class="col d-flex justify-content-center">
+                    <img src="/images/Project.svg" class="img-fluid" alt="" />
                 </div>
 
             </div>
